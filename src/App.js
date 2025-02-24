b1d5c2114f11dce5f684293309f5024f8b808748
Kahssie <73805707+Kahssie@users.noreply.github.com>
2025-02-24 23:18:31 +0800

    Multiple components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
   const title = "Kokaina"
   const likes = 50;
+  const link = "http://www.google.com"
 
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
         <h1>{ title }</h1>
-        <p>Liked { likes } times</p>
+        <Home />
       </div>
     </div>
   );
