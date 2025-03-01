4f023e602e28b0a67ee3cdc6dc340beb9fa967c9
Kahssie <73805707+Kahssie@users.noreply.github.com>
2025-03-01 17:59:36 +0800

    Adding useStates to button onClick events.

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,25 @@
-const Home = () => {
+import { useState } from "react";
 
-	const handleClick = (e) => {
-		console.log('hello smelly bois', e);
+const Home = () => {
+	// let name = 'Kahssie';
+
+  const [name, setName]	= useState('Kahssie');
+	const [age, setAge] = useState(25);
+
+	const handleClick = () => {
+		// console.log('hello smelly bois', e); e is default returned param from events
+		setName('Luigi');
+		setAge(26);
 	};
 
-	const handleClickAgain = (name, e) => {
-		console.log('hello '+ name, e.target);
-	}
+
 
 	return (
 		<div className="home">
 			<h2>Homepage</h2>
+			<p>{name} is {age} years old.</p>
 			<button onClick={handleClick}>Click me</button>
-			<button onClick={(e) => handleClickAgain('mario', e)}>Click me again</button>
+			{/* <button onClick={(e) => handleClickAgain('mario', e)}>Click me again</button> */}
 		</div>
 	);
 }
