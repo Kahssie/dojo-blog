6b837668f341d0237b06ca142e4125e081adc259
Kahssie <73805707+Kahssie@users.noreply.github.com>
2025-03-01 18:33:41 +0800

    Dynamic list handling

--- src/Home.js
+++ src/Home.js
@@ -1,25 +1,20 @@
 import { useState } from "react";
 
 const Home = () => {
-	// let name = 'Kahssie';
-
-  const [name, setName]	= useState('Kahssie');
-	const [age, setAge] = useState(25);
-
-	const handleClick = () => {
-		// console.log('hello smelly bois', e); e is default returned param from events
-		setName('Luigi');
-		setAge(26);
-	};
-
-
+	const [blogs, setBlogs] = useState([
+		{ title: 'HAVANA', body: 'ONANA', author:'Camilla', id: 1 },
+		{ title: 'The BFG', body: 'BIG FUCKEN GIANT', author:'Ralph', id: 2 },
+		{ title: 'Surrounded by Idiots', body: 'not rlly no', author:'Strikson', id: 3 }
+	])
 
 	return (
 		<div className="home">
-			<h2>Homepage</h2>
-			<p>{name} is {age} years old.</p>
-			<button onClick={handleClick}>Click me</button>
-			{/* <button onClick={(e) => handleClickAgain('mario', e)}>Click me again</button> */}
+			{ blogs.map((blog) => (
+				<div className="blog-preview" key={blog.id}>
+					<h2>{blog.title}</h2>
+					<p>By {blog.author}</p>
+				</div>
+			)) }
 		</div>
 	);
 }
