60bc83c93eb1d59fdc9919cee5e567e36108536b
Kahssie <73805707+Kahssie@users.noreply.github.com>
2025-03-02 13:55:53 +0800

    Add Fetch error handling

--- src/Home.js
+++ src/Home.js
@@ -6,24 +6,38 @@ import BlogList from "./BlogList";
 const Home = () => {
 	const [blogs, setBlogs] = useState(null);
 	const [isPending, setIsPending] = useState(true);
+	const [error, setError] = useState(null);
 
 
 	const [name, setName] = useState('Ralph');
 
 	useEffect(() => {
-		fetch('http://localhost:8000/blogs')
+		setTimeout(()=>{
+			fetch('http://localhost:8000/blogs')
 			.then(res => {
+				console.log(res);
+				if(!res.ok){
+					throw Error('Could not fetch the data for this resource.');
+				}
 				return res.json();
 			})
 			.then((data) => {
 				console.log(data);
 				setBlogs(data);
 				setIsPending(false);
+				setError(null);
 			})
+			.catch((err) => {
+				setError(err.message);
+				console.log(err.message);
+				setIsPending(false);
+			})
+		}, 1000)
 	}, []); // empty dependency array only runs once on the first render, or add any state values that should trigger useeffect when their state changes
 
 	return (
 		<div className="home">
+			{ error && <div>{ error }</div> }
 			{ isPending && <div>Loading...</div>}
 			{blogs && <BlogList blogs={blogs} title="All Blogs✨"/>}
 			<button onClick={() => setName('Luigi')}>Change Name</button>
